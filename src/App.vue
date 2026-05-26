840009f9d943613d1ac81aa4a2e20c261ef904b0
bronty <at3177198@gmail.com>
2026-05-26 23:43:08 +0900

    Feat: Update position handling and area calculation

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,11 @@ import { ref } from 'vue';
   maximumAge: 0,
 };
 
-let positions = ref<GeolocationPosition[]>([]);
+//let positions = ref<GeolocationPosition[]>([]);
+let positions = ref<number[][]>([[]]);
+let TestPosition: number[][] = [[35.69426506378273, 139.74629470257938], [35.67733248152762, 139.74885703005893], [35.67564601093697, 139.75945600414306],[35.68992391968861, 139.76060600375328]]; // 東京、ロサンゼルス、ロンドンの位置
+
+//positions.value = TestPosition;
 
 function savePosition() {
   navigator.geolocation.getCurrentPosition(
@@ -19,7 +23,7 @@ function savePosition() {
       console.log(`緯度: ${crd.latitude}`);
       console.log(`軽度: ${crd.longitude}`);
       console.log(`誤差 ${crd.accuracy} メートル`);
-      positions.value.push(pos);
+      positions.value.push([crd.latitude, crd.longitude]);
     },
     (err) => {
       console.error("位置情報の取得に失敗しました:", err);
@@ -28,6 +32,49 @@ function savePosition() {
   );
 }
 
+function latLngToXY(lat:number, lng:number, baseLat:number, baseLng:number) {
+  const latRad = baseLat * Math.PI / 180;
+
+  const x =
+    (lng - baseLng) *
+    111320 *
+    Math.cos(latRad);
+
+  const y =
+    (lat - baseLat) *
+    111320;
+
+  return { x, y };
+}
+
+function calcAreaSize(){
+  if (positions.value.length < 3) {
+    console.warn("少なくとも3点が必要です。");
+    return 0;
+  }
+  let sum = 0;
+  const baseLat = positions.value[0][0];
+  const baseLng = positions.value[0][1];
+  
+  const points = positions.value.map(pos => 
+    //latLngToXY(pos.coords.latitude, pos.coords.longitude, baseLat!, baseLng!)
+    latLngToXY(pos[0]||0, pos[1]||0, baseLat!, baseLng!)
+  );
+  
+  for (let i = 0; i < points.length; i++) {
+    const j = (i + 1) % points.length;
+
+    const pi = points[i];
+    const pj = points[j];
+    if (pi && pj) {
+      sum += pi.x * pj.y;
+      sum -= pj.x * pi.y;
+    }
+  }
+
+  return Math.abs(sum) / 2;
+
+}
 
 //navigator.geolocation.getCurrentPosition(success, error, options);
 </script>
@@ -42,11 +89,15 @@ function savePosition() {
     <h2>保存された位置情報:</h2>
     <ul>
       <li v-for="(pos, index) in positions" :key="index">
-        緯度: {{ pos.coords.latitude }}, 軽度: {{ pos.coords.longitude }}, 誤差: {{ pos.coords.accuracy }} メートル
+        緯度: {{ pos[0] }}, 経度: {{ pos[1] }}
       </li>
     </ul>
   </div>
   <button @click="savePosition">位置情報を保存</button>
+  <div>
+    <h2>面積:</h2>
+    <p>{{ calcAreaSize() }} 平方メートル</p>
+  </div>
 </template>
 
 <style scoped></style>
